603d47c5ce76136344d0726ff292a99d768bfaa0
Thinh <giathinh961617@gmail.com>
2024-11-21 14:26:32 +0700

    hoan thanh chuong 7

--- Chuong7/Bai020/index.js
+++ Chuong7/Bai020/index.js
@@ -4,14 +4,14 @@ let count = 0;
 let saveEl = document.getElementById("save-el")
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
-
+//khác biệt giữa textContent và innerText
 function save() {
     // 2. Tạo một biến chứa cả giá trị của count và dấu gạch ngang, ví dụ: "12 - "
     let countStr = count + " - " // Biến lưu giá trị "count" và dấu gạch ngang
-    // 3. Hiển thị biến trong saveEl sử dụng innerText
+    // 3. Hiển thị biến trong saveEl sử dụng textContent
     // Lưu ý: Đảm bảo không xóa nội dung hiện tại của đoạn văn
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
     console.log(count);
 }
